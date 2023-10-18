2305125ece47cb84885c98c9647f4d4eba98239b
khepburn <khepburn@unitassoftware.com>
2023-10-18 17:29:03 +0100

    Document Content workaround adminMenu

--- cypress/e2e/adminMenu.cy.js
+++ cypress/e2e/adminMenu.cy.js
@@ -8,6 +8,7 @@ beforeEach(() => {
   cy.navigateToAdminOptions();
 });
 
+//! unitas_background is the class of the body element inside the iFrame pageContent
 //Page is rendered with pageContainer first, then iFrame of pageContent, then and iFrame of content
 describe("visual regression tests for accounts reports", () => {
   it("should take a picture of the admin page on load", () => {
@@ -18,20 +19,45 @@ describe("visual regression tests for accounts reports", () => {
     //cy.get("#AdminOpts").should("be.visible");
     cy.iframe("#pageContent").within(() => {
       cy.get(".unitas_background").should("exist");
-      //cy.get(".unitas_background").its('0.contentDocument.body').
-
-      //cy.get(".unitas_background").children();
-      // const insideUnitasBackground = cy.get(".unitas_background").children();
-      // insideUnitasBackground.should("exist");
     });
 
-    cy.iframe("#pageContent").find(".unitas_background").should("exist");
-    const pageContentiFrame = cy.iframe("#pageContent").find("#document");
-    //!return to docs and start here'
-    cy.enter("#pageContent").then((getiFrameBody) => {
-      getiFrameBody().find(".unitas_background").should("exist");
-      getiFrameBody().find("#AdminOpts").children().should("exist");
-    });
+    // cy.get("#frame1")
+    cy.get("#pageContent")
+      .its("0.contentDocument")
+      .its("body")
+      .find(".unitas_background")
+      .should("exist");
+
+    cy.get("#pageContent")
+      .its("0.contentDocument")
+      .its("body")
+      .find('[name="form"]')
+      .should("exist");
+
+    //finds the table that stores the admin options
+    cy.get("#pageContent")
+      .its("0.contentDocument")
+      .its("body")
+      .find("#AdminOpts")
+      .should("exist");
+
+    //finds the anchor elements that store the admin options
+    //(20 items text tags match what is displayed in table aa clickable options)
+    cy.get("#pageContent")
+      .its("0.contentDocument")
+      .its("body")
+      .find("#AdminOpts")
+      .find("td")
+      .find("a")
+      .should("exist");
+
+    cy.wait(2000);
+    //!a href="../shared/useradmin_sel.asp?action=0&usr=unitassup"
+
+    //todo: check iFrame plugin for cypress docs
+    cy.iframe("#pageContent").get(".unitas_background").should("exist");
+
+    cy.enter("#pageContent").then((getiFrameBody) => {});
   });
 
   //end of describe scope
